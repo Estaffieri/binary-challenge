11ed1bf926544222e99131d6f8ec4320a89cb218
Estelle Staffieri <estellelareinne@gmail.com>
2021-01-14 16:51:02 -0700

    Create components folders and connect apis

---
+++ src/apiCalls.js
@@ -0,0 +1,18 @@
+
+export const getAllCategories = () => {
+    return fetch("https://api.publicapis.org/categories")
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+
+export const getCategoryEntries = (category) => {
+    return fetch(`https://api.publicapis.org/entries?category=${category}`)
+        .then(response => response.json())
+        .then(data => console.log(data.title))
+}
+
+export const getRandomApi = () => {
+    return fetch("https://api.publicapis.org/random")
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
