cbc04790c629331631a11b7a971d685e2b8d64b2
Estelle Staffieri <estellelareinne@gmail.com>
2021-01-15 00:54:42 -0700

    Add methods to Home to display all categories

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,29 +1,54 @@
 import React, { Component } from "react";
 import "./Home.css";
-// import { getAllCategories, getCategoryEntries } from "../../apiCalls";
+import ApiDetails from "../ApiDetails/ApiDetails";
+import { getAllCategories, getCategoryEntries } from "../../apiCalls";
 
 
 class Home extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            categories: ["Test", "Test"]
+            categories: [],
+            focus: {}
         }
 
     }
 
-    dropDownMaker() {
-        this.state.categories.map(category => {
-           return <div>{category}</div>
+    componentDidMount() {
+
+        this.setState(getAllCategories())
+        console.log(this.state.categories)
+
+        
+    }
+
+    selectCategory() {
+        getCategoryEntries(this.props.category)
+        .then(selectedCategory => {
+            this.setState.focus({focus: selectedCategory.category})
         })
     }
+
+    displayCategories = () => {
+        const cats = this.state.categories.map(category => {
+            return (<li>{category}</li>)
+        })
+        return cats
+    }
+  
 
 
     render() {
         return (
             <section className="category-container">
                 <h1>"Home"</h1>
-                
+                <nav>
+                    <ul>
+                        {this.displayCategories()}
+                    </ul>
+
+                </nav>
+                <ApiDetails />
 
             </section>
 
