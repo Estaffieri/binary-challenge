f33bc0d0bdc4d8af25a4bf031f8c8708cce19b30
Estelle Staffieri <estellelareinne@gmail.com>
2021-01-16 14:31:21 -0700

    Rebuild Api calls as basic api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,18 +1,19 @@
 
-export const getAllCategories = async () => {
-    const response = await fetch("https://api.publicapis.org/categories")
-    const data = await response.json()
-    return console.log(data)
+export const getAllCategories = () => {
+    return fetch("https://api.publicapis.org/categories")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    
 }
 
-export const getCategoryEntries = async (category) => {
-    const response = await fetch(`https://api.publicapis.org/entries?category=${category}`)
-    const data = await response.json()
-    return console.log(data)
+export const getCategoryEntries = (category) => {
+    return fetch(`https://api.publicapis.org/entries?category=${category}`)
+    .then (response => response.json())
+    .then(data => console.log(data))
 }
 
-export const getRandomApi = async () => {
-    const response = await fetch("https://api.publicapis.org/random?auth=null")
-    const data = await response.json()
-    return console.log(data.entries)
+export const getRandomApi = () => {
+    return fetch("https://api.publicapis.org/random?auth=null")
+    .then(response => response.json())
+    .then(data => console.log(data.entries))
 }
