b4cd6d99fd490b5dca7faf02056e90356bce4dd5
Estelle Staffieri <estellelareinne@gmail.com>
2021-01-18 18:33:29 -0700

    Get all tests passing for DetailCards

--- src/Components/DetailCard/DetailCard.test.js
+++ src/Components/DetailCard/DetailCard.test.js
@@ -1,29 +1,31 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event"
+import  userEvent  from "@testing-library/user-event"
 import DetailCard from "./DetailCard";
 
 
 describe("DetailCards", () => {
+  
     describe("Unit Tests", () => {
+        const mockAddToFavorites = jest.fn()
+        const entry = {
+            API: "Estelle Facts",
+            Description: "Daily Estelle Facts",
+            Auth: "",
+            HTTPS: true,
+            Cors: "no",
+            Link: "https://linktonowhere.com",
+            Category: "Animals"
+        }
+
+        render(
+            <DetailCard entry={entry} key={1}
+                addToFavorites={mockAddToFavorites}
+            />
+        )
 
         it("should render a detail card", () => {
-            const entry = {
-                API: "Estelle Facts",
-                Description: "Daily Estelle Facts",
-                Auth: "",
-                HTTPS: true,
-                Cors: "no",
-                Link: "https://linktonowhere.com",
-                Category: "Animals"
-            }
-
-            const addToFavorites = jest.fn()
-
-            render(<DetailCard entry={entry} key={1}
-                addToFavorites={addToFavorites}
-            />
-            )
+           
             expect(screen.getByText("Estelle Facts")).toBeInTheDocument()
             expect(screen.getByText("Daily Estelle Facts")).toBeInTheDocument()
             expect(screen.getByText("no")).toBeInTheDocument()
@@ -33,6 +35,7 @@ describe("DetailCards", () => {
         })
 
         it("should render with an img", () => {
+            const mockAddToFavorites = jest.fn()
             const entry = {
                 API: "Estelle Facts",
                 Description: "Daily Estelle Facts",
@@ -43,14 +46,51 @@ describe("DetailCards", () => {
                 Category: "Animals"
             }
 
-            const addToFavorites = jest.fn()
-
-            render(<DetailCard entry={entry} key={1}
-                addToFavorites={addToFavorites}
-            />
+            render(
+                <DetailCard entry={entry} key={1}
+                    addToFavorites={mockAddToFavorites}
+                />
             )
+          
+            expect(screen.getByText("Estelle Facts")).toBeInTheDocument()
+            expect(screen.getByText("Daily Estelle Facts")).toBeInTheDocument()
+            expect(screen.getByText("no")).toBeInTheDocument()
+            expect(screen.getByText("https://linktonowhere.com")).toBeInTheDocument()
+            expect(screen.getByText("Animals")).toBeInTheDocument()
             expect(screen.getByAltText("inactive favorite icon")).toBeInTheDocument()
           
+        })
+    })
+
+    describe("Integration Tests", () => {
+
+        it("Should invoke a function when clicked", () => {
+            const mockAddToFavorites = jest.fn()
+            const entry = {
+                API: "Estelle Facts",
+                Description: "Daily Estelle Facts",
+                Auth: "",
+                HTTPS: true,
+                Cors: "no",
+                Link: "https://linktonowhere.com",
+                Category: "Animals"
+            }
+
+            render(
+            <DetailCard entry={entry} key={1}
+                addToFavorites={mockAddToFavorites}
+            />
+            )
+            
+            const inactiveFav = screen.getByAltText("inactive favorite icon")
+            expect(screen.getByText("Estelle Facts")).toBeInTheDocument()
+            expect(screen.getByText("Daily Estelle Facts")).toBeInTheDocument()
+            expect(screen.getByText("no")).toBeInTheDocument()
+            expect(screen.getByText("https://linktonowhere.com")).toBeInTheDocument()
+            expect(screen.getByText("Animals")).toBeInTheDocument()
+            expect(inactiveFav).toBeInTheDocument()
+            userEvent.click(inactiveFav)
+            expect(mockAddToFavorites).toHaveBeenCalled()
         })
     })
 })
